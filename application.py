86e2c13461772431dcf25837f8d960cfc1950417
jhtimmins <james@tradesy.com>
2017-04-17 12:15:31 -0700

    Pull in basic configs.

--- application.py
+++ application.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 application = Flask(__name__)
+application.config.from_pyfile('config.py')
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(application)
+
+from models import Appropriation
 
 @application.route('/')
 def index():
