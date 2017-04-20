6928ffd1a794efd9dcc8d5d08356b9ec0edc26c9
jhtimmins <james@tradesy.com>
2017-04-20 03:55:48 -0700

    Use base js to handle links.

--- static/base.js
+++ static/base.js
@@ -3,5 +3,17 @@ class Base {
 		$("#logo").on("click", function() {
 			window.location = '/';
 		})
+
+		$("#departments").on("click", function() {
+			window.location = '/departments';
+		})
+
+		$("#mayor").on("click", function() {
+			window.location = '/mayor';
+		})
+
+		$("#data").on("click", function() {
+			window.location = 'https://data.lacity.org/browse?q=budget&sortBy=relevance&utf8=%E2%9C%93';
+		})
 	}
 }
