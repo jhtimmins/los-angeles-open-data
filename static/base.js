62422e0a53337dd553464fe2d4c2a50dfd1cbe1d
jhtimmins <james@tradesy.com>
2017-04-20 06:19:43 -0700

    Handle department mobile.

--- static/base.js
+++ static/base.js
@@ -15,5 +15,36 @@ class Base {
 		$("#data").on("click", function() {
 			window.location = 'https://data.lacity.org/browse?q=budget&sortBy=relevance&utf8=%E2%9C%93';
 		})
+
+		this.handleWidth();
+	}
+
+	handleWidth()
+	{
+		var width = $(window).width();
+		if (width < 800) {
+			$("#chart-svg").attr(
+				{
+					"width": width * 0.75,
+					"heigth": width * 0.45
+				}
+				);
+			$("#department-text-wrapper").css({
+				"margin-top": "250px",
+				"float": "right",
+				"width": width * 0.75
+			}
+			);
+		} else {
+			$("#department-text-wrapper").css({
+				"width": width * 0.25,
+				}
+			);
+			$("#chart-svg").attr(
+				{
+					"width": width * 0.48,
+				}
+			);
+		}
 	}
 }
